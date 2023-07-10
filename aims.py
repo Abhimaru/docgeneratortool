ccce05c2f87f72333108b95f7ecf3e9ee566947b
Abhi Maru <72540047+Abhimaru@users.noreply.github.com>
2023-07-10 19:01:47 +0530

    Intial Commit

---
+++ aims.py
@@ -0,0 +1,6 @@
+aims = {
+    "Lab-1": "Write a program to create a simple hello world application.",
+    "Lab-2a": "Write a program to create a simple calculator application.",
+    "Lab-2b": "Write a program to create a simple calculator application.",
+    "Lab-3": "Write a program to create a simple calculator application."
+}
---
+++ finalCode.py
@@ -0,0 +1,179 @@
+import docx as aw
+from docx.oxml.ns import qn
+import os
+from aims import aims
+
+# Define Doc Details
+heading = ''
+headingSize = 20
+aimSize = 16
+paraSize = 12
+fontOne = "Calibri"
+fontTwo = "Consolas"
+
+# Create a new document
+doc = aw.Document()
+
+# Set Document Margins to normal
+sections = doc.sections
+for section in sections:
+    section.top_margin = aw.shared.Inches(1)
+    section.bottom_margin = aw.shared.Inches(1)
+    section.left_margin = aw.shared.Inches(1)
+    section.right_margin = aw.shared.Inches(1)
+
+
+def makedoc(heading, aim, headingSize, aimSize, paraSize, folderName, filesNames):
+    '''
+    HEADINGS
+    '''
+    pg1 = doc.add_paragraph()
+    pg1.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    pg1 = pg1.add_run(heading)
+    pg1.font.name = fontOne
+    pg1.font.bold = True
+    pg1.font.underline = True
+    pg1.font.size = aw.shared.Pt(headingSize)
+    pg1.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    '''
+    AIM
+    '''
+    pg2 = doc.add_paragraph()
+    pg2.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    pg2 = pg2.add_run(f'Aim: {aim}')
+    pg2.font.name = fontOne
+    pg2.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+    pg2.font.bold = True
+    pg2.font.size = aw.shared.Pt(aimSize)
+    pg2.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    # Read File and add to document
+    for fileName in filesNames:
+        pg3 = doc.add_paragraph()
+        pg3.style = 'List Bullet'
+        pg3 = pg3.add_run(fileName)
+        pg3.font.underline = True
+        pg3.font.bold = True
+        pg3.font.size = aw.shared.Pt(aimSize-2)
+        with open(f'{folderName}/{fileName}', 'r') as f:
+            code = f.read()
+        pg4 = doc.add_paragraph()
+        pg4.paragraph_format.space_after = aw.shared.Pt(15)
+        pg4 = pg4.add_run(code)
+        pg4.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+        pg4.font.name = fontTwo
+        pg4.font.size = aw.shared.Pt(paraSize)
+        pg4.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+        pg4.font.bold = False
+
+    # Add Page Break
+    doc.add_page_break()
+    pg5 = doc.add_paragraph().add_run("OUTPUT:")
+    pg5.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.LEFT
+    pg5.font.name = fontOne
+    pg5.font.bold = True
+    pg5.font.underline = True
+    pg5.font.size = aw.shared.Pt(aimSize)
+    pg5.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    # Add Screenshot to document
+    tmp = folderName+'/output/'
+    for image in os.listdir(tmp):
+        image_name = os.path.basename(image)
+        doc.add_picture(tmp+image_name, width=aw.shared.Inches(3.13))
+
+    doc.add_page_break()
+
+
+java_files = []
+xml_files = []
+other_files = []
+filesNames = []
+folderNames = []
+parentFolderName = "ALL LABS"
+
+
+for folder in os.listdir(parentFolderName):
+    # If folder is directory then append it to folderNames
+    if os.path.isdir(os.path.join(parentFolderName, folder)):
+        folderNames.append(folder)
+
+# Set Current Directory to parentFolderName
+os.chdir(parentFolderName)
+
+op = input("1. For Single LAB\n2. For All LAB\nChoose options (1/2):")
+if op == '1':
+    print("Lab No\tLab Name")
+    for i in range((len(folderNames))):
+        print(f'{i+1}\t{folderNames[i]}')
+    labNo = int(
+        input("Enter Lab No: (Any Number from above list,Otherwise it will be exit) : "))
+    if labNo > len(folderNames) or labNo < 1:
+        print("Invalid Input")
+        exit()
+    else:
+        folderName = folderNames[labNo-1]
+        heading = folderName
+        aim = aims[folderName]
+        for file in os.listdir(folderName):
+            file_name = os.path.join(folderName, file)
+            if os.path.isfile(file_name):
+                if file.endswith('.java') and file != 'MainActivity.java':
+                    java_files.append(file)
+                elif file.endswith('.java') and file == 'MainActivity.java':
+                    java_files.insert(0, file)
+                elif file.endswith('.xml') and file != 'activity_main.xml':
+                    xml_files.append(file)
+                elif file.endswith('.xml') and file == 'activity_main.xml':
+                    xml_files.insert(0, file)
+                else:
+                    other_files.append(file)
+
+            filesNames = java_files + xml_files + other_files
+
+        # call makedoc function
+        makedoc(heading, aim, headingSize, aimSize,
+                paraSize, folderName, filesNames)
+        java_files.clear()
+        xml_files.clear()
+        other_files.clear()
+        filesNames.clear()
+
+elif op == '2':
+    for folderName in folderNames:
+        heading = folderName
+        aim = aims[folderName]
+        for file in os.listdir(folderName):
+            file_name = os.path.join(folderName, file)
+            if os.path.isfile(file_name):
+                if file.endswith('.java') and file != 'MainActivity.java':
+                    java_files.append(file)
+                elif file.endswith('.java') and file == 'MainActivity.java':
+                    java_files.insert(0, file)
+                elif file.endswith('.xml') and file != 'activity_main.xml':
+                    xml_files.append(file)
+                elif file.endswith('.xml') and file == 'activity_main.xml':
+                    xml_files.insert(0, file)
+                else:
+                    other_files.append(file)
+
+            filesNames = java_files + xml_files + other_files
+
+        # print(f'Folder Name: {folderName}')
+        # print(f'Files: {filesNames}')
+        # call makedoc function
+        makedoc(heading, aim, headingSize, aimSize,
+                paraSize, folderName, filesNames)
+        java_files.clear()
+        xml_files.clear()
+        other_files.clear()
+        filesNames.clear()
+
+else:
+    print("Invalid Input")
+    exit()
+# Save document
+doc.save('test.docx')
+# Open Document
+os.system(f'start test.docx')
