8497eccbca92fa82f718f0998f5a8133f577658e
Abhi Maru <72540047+Abhimaru@users.noreply.github.com>
2023-07-18 11:03:05 +0530

    Document of Individual Files can be possible to create

--- aims.py
+++ aims.py
@@ -1,3 +1,4 @@
 aims = {
-    "Lab-1": "Write a program to create a simple hello world application.",
+    "EXPERIMENT-1": "Write a program to create a simple hello world application.",
+    "EXPERIMENT-2": "Write a program to create a simple hello world application.",
 }
--- generateDoc.py
+++ generateDoc.py
@@ -205,6 +205,75 @@ def makedoc(heading, aim, headingSize, aimSize, paraSize, folderName, filesNames
         doc.add_section().header.is_linked_to_previous = False
 
 
+def makedocIndividual(heading, aim, headingSize, aimSize, paraSize, image, file, isLastPage=False):
+    '''
+    HEADINGS
+    '''
+    pg1 = doc.add_heading()
+    pg1.underline = aw.shared   .RGBColor(0, 0, 0)
+    pg1.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    pg1.style = doc.styles['Title']
+    pg1 = pg1.add_run(heading)
+    pg1.font.name = fontOne
+    pg1.font.bold = True
+    pg1.font.size = aw.shared.Pt(headingSize)
+    pg1.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    '''
+    AIM
+    '''
+    pg2 = doc.add_heading()
+    pg2.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    pg2 = pg2.add_run(f'Aim: {aim}')
+    pg2.font.name = fontOne
+    pg2.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+    pg2.font.bold = True
+    pg2.font.size = aw.shared.Pt(aimSize)
+    pg2.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    # Read File and add to document
+    pg3 = doc.add_paragraph()
+    pg3.paragraph_format.space_before = aw.shared.Pt(15)
+    pg3 = pg3.add_run("CODE:")
+    pg3.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.LEFT
+    pg3.font.name = fontOne
+    pg3.font.bold = True
+    pg3.font.underline = True
+    pg3.font.size = aw.shared.Pt(aimSize-2)
+    pg3.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    with open(file, 'r') as f:
+        code = f.read()
+    pg4 = doc.add_paragraph()
+    pg4.paragraph_format.space_after = aw.shared.Pt(15)
+    pg4 = pg4.add_run(code)
+    pg4.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    pg4.font.name = fontTwo
+    pg4.font.size = aw.shared.Pt(paraSize)
+    pg4.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+    pg4.font.bold = False
+
+    # Add Page Break
+    pg5 = doc.add_paragraph().add_run("OUTPUT:")
+    pg5.alignment = aw.enum.text.WD_ALIGN_PARAGRAPH.LEFT
+    pg5.font.name = fontOne
+    pg5.font.bold = True
+    pg5.font.underline = True
+    pg5.font.size = aw.shared.Pt(aimSize)
+    pg5.font.color.rgb = aw.shared.RGBColor(0, 0, 0)
+
+    # Add Screenshot to document
+    tmp = 'outputs/'
+    add_image_border(tmp+image, f'{tmp}tmp_{image}', image_border_size)
+    doc.add_picture(f'{tmp}tmp_{image}', width=aw.shared.Inches(3.13))
+    os.remove(f'{tmp}tmp_{image}')
+
+    if not isLastPage:
+        doc.add_page_break()
+        # add new section
+        doc.add_section().header.is_linked_to_previous = False
+
+
 java_files = []
 xml_files = []
 other_files = []
@@ -212,96 +281,156 @@ filesNames = []
 folderNames = []
 parentFolderName = "LABS"
 
-makeFolder(parentFolderName)
+opt = input(
+    "1. For individual files\n2. For individual folders\nChoose options (1/2):")
+if opt == '1':
+    os.chdir("LABS")
+    if not os.path.exists("outputs"):
+        os.mkdir("outputs")
 
-for folder in os.listdir(parentFolderName):
-    # If folder is directory then append it to folderNames
-    if os.path.isdir(os.path.join(parentFolderName, folder)):
-        folderNames.append(folder)
-# Check if it is last lab or not
-isLastLab = False
-# Set Current Directory to parentFolderName
-os.chdir(parentFolderName)
-op = input("1. For Single LAB\n2. For All LAB\nChoose options (1/2):")
-if op == '1':
-    print("Lab No\tLab Name")
-    for i in range((len(folderNames))):
-        print(f'{i+1}\t{folderNames[i]}')
-    labNo = int(
-        input("Enter Lab No: (Any Number from above list,Otherwise it will be exit) : "))
-    if labNo > len(folderNames) or labNo < 1:
-        print("Invalid Input")
-        exit()
-    else:
-        isLastLab = True
-        folderName = folderNames[labNo-1]
-        heading = folderName
-        aim = aims[folderName] if aims.keys().__contains__(folderName) else ""
-        for file in os.listdir(folderName):
-            file_name = os.path.join(folderName, file)
-            if os.path.isfile(file_name):
-                if file.endswith('.java') and file != 'MainActivity.java':
-                    java_files.append(file)
-                elif file.endswith('.java') and file == 'MainActivity.java':
-                    java_files.insert(0, file)
-                elif file.endswith('.xml') and file != 'activity_main.xml':
-                    xml_files.append(file)
-                elif file.endswith('.xml') and file == 'activity_main.xml':
-                    xml_files.insert(0, file)
-                else:
-                    other_files.append(file)
+    def sort_file(file_list):
+        return sorted(file_list, key=lambda x: int(x.split('-')[0]))
 
-            filesNames = java_files + xml_files + other_files
+    def sort_img(img_list):
+        return sorted(img_list, key=lambda x: int(x.split('.')[0]))
 
-        # call makedoc function
-        makedoc(heading, aim, headingSize, aimSize,
-                paraSize, folderName, filesNames, isLastLab)
-        folderName = [folderName]
-        java_files.clear()
-        xml_files.clear()
-        other_files.clear()
-        filesNames.clear()
-        txt = input("do you want to add Header? (y/n)")
-        if (txt == 'y' or txt == 'Y'):
-            makeHeader(folderName)
+    file_list = []
+    img_list = []
+    i = 1
+    for file in os.listdir():
+        if os.path.isfile(file):
+            file_list.append(file)
+        mystr = f'{i}.png'
+        img_list.append(mystr)
+        i += 1
 
+    img_list = sort_img(img_list)
+    file_list = sort_file(file_list)
+    count = 1
+    heading_list = []
+    tmp = 0
+    for file in file_list:
+        try:
+            if not tmp == len(file_list):
+                img = img_list[tmp]
 
-elif op == '2':
-    for folderName in folderNames:
-        heading = folderName
-        aim = aims[folderName] if aims.keys().__contains__(folderName) else ""
-        for file in os.listdir(folderName):
-            file_name = os.path.join(folderName, file)
-            if os.path.isfile(file_name):
-                if file.endswith('.java') and file != 'MainActivity.java':
-                    java_files.append(file)
-                elif file.endswith('.java') and file == 'MainActivity.java':
-                    java_files.insert(0, file)
-                elif file.endswith('.xml') and file != 'activity_main.xml':
-                    xml_files.append(file)
-                elif file.endswith('.xml') and file == 'activity_main.xml':
-                    xml_files.insert(0, file)
-                else:
-                    other_files.append(file)
-
-            filesNames = java_files + xml_files + other_files
-
-        if folderName == folderNames[-1]:
-            isLastLab = True
-        # print(f'Folder Name: {folderName}')
-        # print(f'Files: {filesNames}')
-        # call makedoc function
-        makedoc(heading, aim, headingSize, aimSize,
-                paraSize, folderName, filesNames, isLastLab)
-        isLastLab = False
-        java_files.clear()
-        xml_files.clear()
-        other_files.clear()
-        filesNames.clear()
+            heading = f'EXPERIMENT-{count}'
+            heading_list.append(heading)
+            isLastLab = False
+            aim = aims[heading] if aims.keys().__contains__(heading) else ""
+            if file == file_list[-1]:
+                isLastLab = True
+            makedocIndividual(heading, aim, headingSize, aimSize,
+                              paraSize, img, file, isLastLab)
+            count += 1
+            tmp += 1
+        except:
+            print("No image found")
+    img_list.clear()
+    file_list.clear()
     txt = input("do you want to add Header? (y/n)")
     if (txt == 'y' or txt == 'Y'):
-        makeHeader(folderNames)
+        makeHeader(heading_list)
 
+elif opt == '2':
+    parentFolderName = "LABS"
+    makeFolder(parentFolderName)
+
+    for folder in os.listdir(parentFolderName):
+        # If folder is directory then append it to folderNames
+        if os.path.isdir(os.path.join(parentFolderName, folder)):
+            folderNames.append(folder)
+
+    # Set Current Directory to parentFolderName
+    os.chdir(parentFolderName)
+    isLastLab = False
+    op = input("1. For Single LAB\n2. For All LAB\nChoose options (1/2):")
+    if op == '1':
+        print("Lab No\tLab Name")
+        for i in range((len(folderNames))):
+            print(f'{i+1}\t{folderNames[i]}')
+        labNo = int(
+            input("Enter Lab No: (Any Number from above list,Otherwise it will be exit) : "))
+        if labNo > len(folderNames) or labNo < 1:
+            print("Invalid Input")
+            exit()
+        else:
+            isLastLab = True
+            folderName = folderNames[labNo-1]
+            heading = folderName
+            aim = aims[folderName] if aims[folderName] != None else ""
+            for file in os.listdir(folderName):
+                file_name = os.path.join(folderName, file)
+                if os.path.isfile(file_name):
+                    if file.endswith('.java') and file != 'MainActivity.java':
+                        java_files.append(file)
+                    elif file.endswith('.java') and file == 'MainActivity.java':
+                        java_files.insert(0, file)
+                    elif file.endswith('.xml') and file != 'activity_main.xml':
+                        xml_files.append(file)
+                    elif file.endswith('.xml') and file == 'activity_main.xml':
+                        xml_files.insert(0, file)
+                    else:
+                        other_files.append(file)
+
+                filesNames = java_files + xml_files + other_files
+
+            # call makedoc function
+            makedoc(heading, aim, headingSize, aimSize,
+                    paraSize, folderName, filesNames, isLastLab)
+            java_files.clear()
+            xml_files.clear()
+            other_files.clear()
+            filesNames.clear()
+
+        txt = input("do you want to add Header? (y/n)")
+        if (txt == 'y' or txt == 'Y'):
+            makeHeader(folderNames)
+
+    elif op == '2':
+        def sort_folder(file_list):
+            return sorted(file_list, key=lambda x: int(x.split('-')[1]))
+
+        folderNames = sort_folder(folderNames)
+        for folderName in folderNames:
+            heading = folderName
+            aim = aims[folderName] if aims.keys(
+            ).__contains__(folderName) else ""
+            for file in os.listdir(folderName):
+                file_name = os.path.join(folderName, file)
+                if os.path.isfile(file_name):
+                    if file.endswith('.java') and file != 'MainActivity.java':
+                        java_files.append(file)
+                    elif file.endswith('.java') and file == 'MainActivity.java':
+                        java_files.insert(0, file)
+                    elif file.endswith('.xml') and file != 'activity_main.xml':
+                        xml_files.append(file)
+                    elif file.endswith('.xml') and file == 'activity_main.xml':
+                        xml_files.insert(0, file)
+                    else:
+                        other_files.append(file)
+
+                filesNames = java_files + xml_files + other_files
+
+            if folderName == folderNames[-1]:
+                isLastLab = True
+            # print(f'Folder Name: {folderName}')
+            # print(f'Files: {filesNames}')
+            # call makedoc function
+            makedoc(heading, aim, headingSize, aimSize,
+                    paraSize, folderName, filesNames, isLastLab)
+            isLastLab = False
+            java_files.clear()
+            xml_files.clear()
+            other_files.clear()
+            filesNames.clear()
+        txt = input("do you want to add Header? (y/n)")
+        if (txt == 'y' or txt == 'Y'):
+            makeHeader(folderNames)
+
+    else:
+        print("Invalid Input")
+        exit()
 else:
     print("Invalid Input")
     exit()
